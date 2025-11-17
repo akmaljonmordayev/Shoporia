51e1aabaa9a11f770267b8eb470c7fd17adb9757
AlfaCefOf <alfaxcef@gmail.com>
2025-11-17 20:18:11 +0500

    addde register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react"; // <-- добавили React
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
