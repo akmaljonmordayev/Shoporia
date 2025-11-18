7b28c71fc5a2db68391b837ce42bededed462206
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-11-18 16:44:05 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
+  </BrowserRouter>
 );
