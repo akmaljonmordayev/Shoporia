cac8dc5a622722c85b1be0394534adbb85857897
Ibrohimjon707 <ibrohimjonathamjonov@example.com>
2025-12-02 17:27:28 +0500

    push

--- src/features/shop/pages/BlogSingle/BlogSingle.jsx
+++ src/features/shop/pages/BlogSingle/BlogSingle.jsx
@@ -15,14 +15,14 @@ function BlogSingle() {
       <div className="max-w-[800px] font-sans text-gray-900">
         <div className="article-card">
           <h2 className="text-[28px] font-extrabold mb-2 max-w-[810px]">
-            {data?.suntitle}
+            {data?.titleBlog}
           </h2>
 
           <p className="text-sm text-gray-500 mb-4">{data?.date}</p>
           <img
             className="w-[808px] h-[414px] rounded-md object-cover mb-5"
-            src=""
-            alt=""
+            src={data?.imageBlog}
+            alt={data?.titleBlog}
           />
 
           <p className="text-base leading-6 text-gray-800">{data?.Bigdesc}</p>
@@ -52,31 +52,56 @@ function BlogSingle() {
         </div>
       </div>
 
-      <aside className="w-[300px] max-w-[300px]">
-        <h3 className="mb-3 text-lg font-semibold">Categories</h3>
-        <ul className="space-y-2">
-          <li className="max-w-[300px]">Technology Trends and News</li>
-          <li className="max-w-[300px]">Gaming Insights</li>
-          <li className="max-w-[300px]">Security and Privacy</li>
-          <li className="max-w-[300px]">Tech Lifestyle and Productivity</li>
-          <li className="max-w-[300px]">Product Spotlight</li>
-          <li className="max-w-[300px]">How-to Guides and Tutorials</li>
-          <li className="max-w-[300px]">Buying Guides and Tips</li>
-        </ul>
-      </aside>
+      <div>
+        <aside className="w-[300px] max-w-[300px]">
+          <h3 className="pt-[90px] mb-3 text-lg font-semibold">Categories</h3>
+          <ul className="space-y-2">
+            <li className="max-w-[300px]">Technology Trends and News</li>
+            <li className="max-w-[300px]">Gaming Insights</li>
+            <li className="max-w-[300px]">Security and Privacy</li>
+            <li className="max-w-[300px]">Tech Lifestyle and Productivity</li>
+            <li className="max-w-[300px]">Product Spotlight</li>
+            <li className="max-w-[300px]">How-to Guides and Tutorials</li>
+            <li className="max-w-[300px]">Buying Guides and Tips</li>
+          </ul>
+        </aside>
 
-      <style jsx>{`
-        @media (max-width: 560px) {
-          textarea {
-            width: 100% !important;
+        <style jsx>{`
+          @media (max-width: 560px) {
+            textarea {
+              width: 100% !important;
+            }
+            button {
+              width: 40% !important;
+              margin-left: 0 !important;
+              align-self: flex-end;
+            }
           }
-          button {
-            width: 40% !important;
-            margin-left: 0 !important;
-            align-self: flex-end;
-          }
-        }
-      `}</style>
+        `}</style>
+        <div className="mt-[50px]">
+          <h2 className="text-2xl font-semibold mb-[24px]">Tags</h2>
+
+          <div className="flex flex-wrap gap-4 max-w-[492px]">
+  <div className="w-[108px] h-[48px] flex items-center justify-center rounded-[8px] border-2 border-[#0c68f4] text-[#3b82f6] font-inter font-[530] text-[16px] leading-[100%] hover:border-[#002f6c] hover:text-[#002f6c] cursor-pointer">
+    Technology
+  </div>
+  <div className="w-[108px] h-[48px] flex items-center justify-center rounded-[8px] border-2 border-[#0c68f4] text-[#3b82f6] font-inter font-[530] text-[16px] leading-[100%] hover:border-[#002f6c] hover:text-[#002f6c] cursor-pointer">
+    Headset
+  </div>
+  <div className="w-[108px] h-[48px] flex items-center justify-center rounded-[8px] border-2 border-[#0c68f4] text-[#3b82f6] font-inter font-[530] text-[16px] leading-[100%] hover:border-[#002f6c] hover:text-[#002f6c] cursor-pointer">
+    Phone
+  </div>
+  <div className="w-[108px] h-[48px] flex items-center justify-center rounded-[8px] border-2 border-[#0c68f4] text-[#3b82f6] font-inter font-[530] text-[16px] leading-[100%] hover:border-[#002f6c] hover:text-[#002f6c] cursor-pointer">
+    Wireless
+  </div>
+  <div className="w-[108px] h-[48px] flex items-center justify-center rounded-[8px] border-2 border-[#0c68f4] text-[#3b82f6] font-inter font-[530] text-[16px] leading-[100%] hover:border-[#002f6c] hover:text-[#002f6c] cursor-pointer">
+    Apple
+  </div>
+</div>
+
+
+        </div>
+      </div>
     </div>
   );
 }
